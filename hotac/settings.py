f7528b83c7ed317734907b392279f6bc5f00c958
mike <toxotes@gmail.com>
2019-06-03 00:30:45 -0400

    Static root fix

--- hotac/settings.py
+++ hotac/settings.py
@@ -114,4 +114,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = '/home/public/static'
 STATIC_URL = '/static/'
