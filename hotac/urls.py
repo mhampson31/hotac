a2dada7d59bde297a37fbf7db04f95b87deaba5c
mhampson31 <41697985+mhampson31@users.noreply.github.com>
2019-06-22 08:52:18 -0400

    Update urls.py
    Fixed busted merge

--- hotac/urls.py
+++ hotac/urls.py
@@ -1,18 +1,3 @@
-
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
@@ -24,33 +9,4 @@ urlpatterns = [
     path('session/<int:session_id>', views.session_summary, name='session'),
     path('pilot/<int:pilot_id>', views.pilot_sheet, name='pilot'),
     path('campaign/<int:pk>', views.CampaignView.as_view(), name='campaign')
-
-]
-
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-
-from campaign import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('session/<int:session_id>', views.session_summary, name='session'),
-    path('pilot/<int:pilot_id>', views.pilot_sheet, name='pilot'),
-    path('campaign/<int:pk>', views.CampaignView.as_view(), name='campaign')
-
 ]
