3b22d8d642851319f8ac1ca70b37d6c5ca80dc3f
Mike <toxotes+gh@gmail.com>
2020-05-17 11:15:35 -0400

    AI delay
    Add a 250ms delay to the AI choice, to make it more apparent when the same result comes up again

--- campaign/static/js/ai.js
+++ campaign/static/js/ai.js
@@ -1,8 +1,10 @@
 $(function() {
    $('button').click(function() {
-   $(this).parent().parent().find('.list-group-item-secondary').removeClass('list-group-item-secondary');
-   var choices = $(this).next();
-   var maneuver = Math.floor(Math.random() * Math.floor(6))
-   choices.children().eq(maneuver).toggleClass('list-group-item-secondary');
+       $(this).parent().parent().find('.list-group-item-secondary').removeClass('list-group-item-secondary');
+       var choices = $(this).next();
+       setTimeout(function() {
+           var maneuver = Math.floor(Math.random() * Math.floor(6))
+           choices.children().eq(maneuver).toggleClass('list-group-item-secondary');
+       }, 250);
    });
 });
