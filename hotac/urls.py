fadbae9df0042f9283b3842f2d2480c6f50e9ce3
Mike <toxotes+gh@gmail.com>
2020-05-18 20:57:08 -0400

    Slugify urls

--- hotac/urls.py
+++ hotac/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('pilot/<int:pilot_id>', views.pilot_sheet, name='pilot'),
     path('campaign/<int:pk>', views.CampaignView.as_view(), name='campaign'),
     path('ses/<int:session_id>', views.old_session_summary, name='old_ses'),
-    path('ship/<int:ship_id>', views.ship_sheet, name='ship'),
-    path('ship/<int:ship_id>/ai', views.ai_select, name='ai'),
+    path('ship/<slug:ship_slug>/', views.ship_sheet, name='ship'),
+    path('ship/<slug:ship_slug>/ai/', views.ai_select, name='ai'),
     path('chaining/', include('smart_selects.urls')),
 ]
