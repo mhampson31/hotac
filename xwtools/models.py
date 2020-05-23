7d05fbd0ea39e2479506e42bb8754e13bfa28f8c
Mike <toxotes+gh@gmail.com>
2020-05-23 19:16:55 -0400

    Fix dial mirror

--- xwtools/models.py
+++ xwtools/models.py
@@ -170,7 +170,7 @@ class DialManeuver(models.Model):
             return self
         else:
             new_direction = {'L':'R', 'R':'L'}[self.direction]
-            return self.dial.dialmaneuver_set.get(speed=self.speed, move=self.bearing, direction=new_direction)
+            return self.dial.maneuvers.get(speed=self.speed, move=self.bearing, direction=new_direction)
 
     @property
     def css_name(self):
