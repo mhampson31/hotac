1132f0ac6595fa7e85566bb1fe4babf1c5713831
Michael Hampson <toxotes+gh@gmail.com>
2022-07-13 12:59:37 -0400

    Moved model test instances to self attributes

--- xwtools/tests.py
+++ xwtools/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from .models import Chassis, Faction, Card, SlotChoice
+from .models import Chassis, Faction, Card, SlotChoice, Dial, DialManeuver
 
 # Create your tests here.
 
@@ -38,26 +38,51 @@ class CardTestCase(TestCase):
         Card.objects.create(name="Pilot", type=SlotChoice.PILOT, initiative=4, chassis=boat)
         Card.objects.create(name="Force Pilot", type=SlotChoice.PILOT, initiative=4, force=True, charges=2, chassis=boat)
 
+        self.droid = Card.objects.get(name="T3-ST")
+        self.talent = Card.objects.get(name="Talent")
+        self.force = Card.objects.get(name="Force Upgrade")
+        self.pilot = Card.objects.get(name="Pilot")
+        self.jedi = Card.objects.get(name="Force Pilot")
+
 
     def test__str(self):
-        droid = Card.objects.get(name="T3-ST")
-        talent = Card.objects.get(name="Talent")
-        jedi = Card.objects.get(name="Force Pilot")
-
-        self.assertEqual(droid.__str__(), '•T3-ST')
-        self.assertEqual(talent.__str__(), 'Talent')
-        self.assertEqual(jedi.__str__(), 'Force Pilot - CardBoat')
-
+        self.assertEqual(self.droid.__str__(), '•T3-ST')
+        self.assertEqual(self.talent.__str__(), 'Talent')
+        self.assertEqual(self.jedi.__str__(), 'Force Pilot - CardBoat')
 
     def test_cost(self):
-        droid = Card.objects.get(name="T3-ST")
-        talent = Card.objects.get(name="Talent")
-        force = Card.objects.get(name="Force Upgrade")
-        pilot = Card.objects.get(name="Pilot")
-        jedi = Card.objects.get(name="Force Pilot")
+        # point costs are complex, depending on card type, Force abilities, and so on
+        self.assertEqual(self.droid.campaign_cost(1), 7) # cost x1
+        self.assertEqual(self.talent.campaign_cost(1), 4) # cost x2
+        self.assertEqual(self.force.campaign_cost(1), 6) # cost x2
+        self.assertEqual(self.pilot.campaign_cost(1), 8) # initiatve x2
+        self.assertEqual(self.jedi.campaign_cost(1), 20) # initiatve x (charges + 3)
 
-        self.assertEqual(droid.campaign_cost(1), 7) # cost x1
-        self.assertEqual(talent.campaign_cost(1), 4) # cost x2
-        self.assertEqual(force.campaign_cost(1), 6) # cost x2
-        self.assertEqual(pilot.campaign_cost(1), 8) # initiatve x2
-        self.assertEqual(jedi.campaign_cost(1), 20) # initiatve x (charges + 3)
+
+class DialTestCase(TestCase):
+    def setUp(self):
+        Chassis.objects.create(name="Dial Shuttle")
+        shuttle = Chassis.objects.get(name="Dial Shuttle")
+
+        Dial.objects.create(chassis=shuttle)
+        self.dial = Dial.objects.first()
+
+        Dial.objects.create(chassis=None)
+        self.other_dial = Dial.objects.last()
+
+    def test__str(self):
+        self.assertEqual(self.dial.__str__(), "Dial Shuttle")
+        self.assertEqual(self.other_dial.__str__(), "--")
+
+    def test_css_name(self):
+        # tests that we get the chassis name lower-cased and stripped of non-letters
+        self.assertEqual(self.dial.css_name, "dialshuttle") #re.sub(r'[^\w\d]', ''
+
+    def test_max_speed(self):
+        # tests that we get the greatest speed value on the dial
+        self.assertEqual(self.dial.max_speed, 1)
+
+    def test_dial_width(self):
+        # tests that we get the number of maneuvers of the speed with the most of them
+        self.assertEqual(self.dial.dial_width, 1)
+
