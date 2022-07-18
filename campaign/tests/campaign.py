3de611509fc5ddb21716845e0c571b283acbc6a0
Michael Hampson <toxotes+gh@gmail.com>
2022-07-18 12:29:09 -0400

    Ally and initial Flight Group tests

--- campaign/tests/campaign.py
+++ campaign/tests/campaign.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 from xwtools.models import Chassis
-from campaign.models import PlayableShip, MissionFeature, Campaign
+from campaign.models import PlayableShip, MissionFeature, Campaign, FlightGroup, FGSetup, Ally
 
 from .utilities import create_rulebook_data, create_mission_data
 
@@ -89,11 +89,80 @@ class CampaignTest(TestCase):
     def test_starting_deck(self):
         self.assertQuerysetEqual(self.campaign.deck.all(), [self.mission,])
 
-
     def test_draw_missions(self):
         # if the starter mission is in the deck, return it
         # if only one thing is in the deck, return it
         # else, return two options
         # set deck_draw to that
         pass
+
+
+
+class FlightGroupTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        mission = create_mission_data(rulebook=create_rulebook_data("Test Rules"))
+        cls.flight_group = FlightGroup.objects.create(name="Alpha", mission=mission, arrival=1, vector=4)
+
+    def test__str(self):
+        self.assertEqual(self.flight_group.__str__, 'Alpha')
+
+    def generate(self):
+        # return a list with the correct enemies given the number of players and their group init
+        # should test replacing default enemies with elite enemies
+        # self.flight_group.generate(self, pilots, group_init)
+        pass
+
+    def batch_load(self):
+        """setup_list should be a list of 6 tuples in (chassis_id, init, action)
+           any can be null, we just make some assumptions for those.
+           The index of each tuple corresponds to the player count.
+        """
+        # batch_load(self, setup_list)
+        pass
+
+
+class FGSetupTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        flight_group = FlightGroup.objects.create(name="Alpha", mission=mission, arrival=1, vector=4)
+        mission = create_mission_data(rulebook=create_rulebook_data("Test Rules"))
+
+        # A generic default enemy that always appears
+        cls.fg_generic = FGSetup(chassis=mission.enemy_faction.default_chassis)
+
+        # a random replacement for 3+ init and 2+ players
+        cls.fg_replacement = FGSetup(action='R', chassis=None, players=2, init=3)
+
+        # a second generic that appears at init 4+
+        cls.fg_extra = FGSetup(chassis=mission.enemy_faction.default_chassis, init=4)
+
+    def test__str(self):
+        self.assertEqual(self.fg_generic.__str__(), 'T-Wing')
+        self.assertEqual(self.fg_replacement.__str__(), 'Random')
+
+    def test_default(self):
+        self.assertTrue(self.fg_generic.is_default)
+        self.assertFalse(self.fg_replacement.is_default)
+
+    def generate(self, group_init):
+        pass
+
+
+class AllyTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        chassis = Chassis.objects.create(name="Ally Shuttle")
+        mission = create_mission_data(rulebook=create_rulebook_data("Test Rules"))
+        cls.ally = Ally.objects.create(mission=mission, chassis=chassis, callsign="Tester")
+
+    def test_str(self):
+        self.assertEqual(self.ally.__str__(), 'Tester')
+
+    def test_pilot(self):
+        self.assertEqual(self.ally.pilot, 'Tester (Ally Shuttle)')
+        return '{} ({})'.format(self.callsign, self.chassis)
 
